3858872e296c1d08b05b0a94aa183dc8d076d684
madhu <077bct042.madhu@pcampus.edu.np>
2025-07-03 21:37:39 +0545

    Workflows updated

---
+++ .github/scripts/is_changed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=$1
+BASE_BRANCH=$2
+EXCLUDED_FILES=(
+  ":!$DIR/README.md",
+  ":!$DIR/.gitignore",
+)
+
+git fetch origin "$BASE_BRANCH" --depth=1 > /dev/null 2>&1
+git diff --quiet origin/"$BASE_BRANCH" -- "$DIR" "${EXCLUDED_FILES[@]}"
