eb81374d66e3e15ae0e23b7738a7f0a6dbe2395c
madhu <077bct042.madhu@pcampus.edu.np>
2025-07-04 07:49:20 +0545

    fix:workflow updated

--- .github/scripts/is_changed.sh
+++ .github/scripts/is_changed.sh
@@ -3,9 +3,9 @@
 DIR=$1
 BASE_BRANCH=$2
 EXCLUDED_FILES=(
-  ":!$DIR/README.md",
-  ":!$DIR/.gitignore",
+  ":!$DIR/README.md"
+  ":!$DIR/.gitignore"
 )
 
 git fetch origin "$BASE_BRANCH" --depth=1 > /dev/null 2>&1
-git diff --name-only origin/"$BASE_BRANCH" -- "$DIR" "${EXCLUDED_FILES[@]}"
+git diff origin/"$BASE_BRANCH" -- "$DIR" "${EXCLUDED_FILES[@]}"
